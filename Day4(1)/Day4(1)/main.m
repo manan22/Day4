10730cb16edd0c6608e90ddae77b2a97ed5e0b4a
manan22 <mananpandya04@yahoo.com>
2014-05-08 14:55:25 -0400

    Day4ex1

---
+++ Day4(1)/Day4(1)/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Day4(1)
+//
+//  Created by macadmin on 2014-05-08.
+//  Copyright (c) 2014 Manan Pandya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
